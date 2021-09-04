f1f27724e926c6d1e2a2ad8deadd610bb10461cd
Ankit Kumar Nayak <ankitnayak882@gmail.com>
2021-09-05 00:45:27 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import './App.css';
+import About from './components/About';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import React, { useState } from 'react';
+
+
+function App() {
+
+  const [ mode , setMode] = useState('light'); //Whether Dark Mode is Enabled or not
+
+  const toggleMode = () => {
+    if(mode === 'light') {
+      setMode('dark')
+      document.body.style.backgroundColor = 'grey';
+    }
+    else {
+      setMode('light')
+      document.body.style.backgroundColor = 'white';
+    }
+  }
+
+  return (
+    <>
+      <Navbar title="TextUtils" aboutText="About TextUtils" mode={mode} toggleMode={toggleMode}/>
+      <div className="container my-3">
+      <TextForm heading="Enter the text to analyze below" mode={mode}/>   
+      {/* <About/> */}
+      </div>
+    </>
+  );
+}
+
+export default App;
